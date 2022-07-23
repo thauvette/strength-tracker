00d72e4b43aabe06c4ae9629b15dbc35495ffaec
thauvette <thauvette@gmail.com>
2022-07-23 09:06:53 -0300

    another build fix

--- src/hooks/useWeightSettings.js
+++ src/hooks/useWeightSettings.js
@@ -18,82 +18,89 @@ const defaultSettings = {
   availablePlates: defaultPlates,
 }
 
-const initialSettings =
+let initialSettings = defaultSettings
+if (
   window !== undefined &&
   window?.localStorage &&
   localStorage.getItem(LOCAL_STORAGE_PLATE_SETTINGS)
-    ? JSON.parse(localStorage.getItem(LOCAL_STORAGE_PLATE_SETTINGS))
-    : defaultSettings
-
-const useWeightSettings = () => {
-  const [settings, setSettings] = useState(initialSettings)
-
-  if (window === undefined) {
-    return null
-  }
-
-  const updateStorage = newSettings => {
-    localStorage.setItem(
-      LOCAL_STORAGE_PLATE_SETTINGS,
-      JSON.stringify({ ...newSettings, updated: new Date().getTime() })
-    )
-  }
-
-  const updateBarWeight = weight => {
-    const currentSettings = { ...settings, barWeight: weight }
-    setSettings({ ...currentSettings })
-    updateStorage(currentSettings)
-  }
-
-  const togglePlateAvailability = index => {
-    const isChecked = get(
-      settings,
-      ["availablePlates", index, "available"],
-      false
-    )
-    const result = set(
-      settings,
-      ["availablePlates", index, "available"],
-      !isChecked
-    )
-    setSettings({ ...result })
-    updateStorage(result)
-  }
-
-  const updatePlateCount = ({ index, count }) => {
-    const result = set(settings, ["availablePlates", index, "count"], count)
-    setSettings({ ...result })
-    updateStorage(result)
-  }
-
-  const addPlate = ({ weight, count }) => {
-    const currentPlates = get(settings, "availablePlates", [])
-
-    currentPlates.push({
-      weight,
-      count,
-      available: true,
-    })
-    const result = {
-      ...settings,
-      availablePlates: currentPlates.sort((a, b) =>
-        a.weight > b.weight ? -1 : 1
-      ),
-    }
-    setSettings(result)
-    updateStorage(result)
-  }
-
-  return {
-    barWeight: settings.barWeight,
-    availablePlates: settings.availablePlates,
-    weightSettings: settings,
-    updateBarWeight,
-    togglePlateAvailability,
-    updatePlateCount,
-    updated: settings.updated,
-    addPlate,
-  }
+) {
+  initialSettings = JSON.parse(
+    localStorage.getItem(LOCAL_STORAGE_PLATE_SETTINGS)
+  )
 }
+
+const useWeightSettings =
+  window === undefined
+    ? null
+    : () => {
+        const [settings, setSettings] = useState(initialSettings)
+
+        const updateStorage = newSettings => {
+          localStorage.setItem(
+            LOCAL_STORAGE_PLATE_SETTINGS,
+            JSON.stringify({ ...newSettings, updated: new Date().getTime() })
+          )
+        }
+
+        const updateBarWeight = weight => {
+          const currentSettings = { ...settings, barWeight: weight }
+          setSettings({ ...currentSettings })
+          updateStorage(currentSettings)
+        }
+
+        const togglePlateAvailability = index => {
+          const isChecked = get(
+            settings,
+            ["availablePlates", index, "available"],
+            false
+          )
+          const result = set(
+            settings,
+            ["availablePlates", index, "available"],
+            !isChecked
+          )
+          setSettings({ ...result })
+          updateStorage(result)
+        }
+
+        const updatePlateCount = ({ index, count }) => {
+          const result = set(
+            settings,
+            ["availablePlates", index, "count"],
+            count
+          )
+          setSettings({ ...result })
+          updateStorage(result)
+        }
+
+        const addPlate = ({ weight, count }) => {
+          const currentPlates = get(settings, "availablePlates", [])
+
+          currentPlates.push({
+            weight,
+            count,
+            available: true,
+          })
+          const result = {
+            ...settings,
+            availablePlates: currentPlates.sort((a, b) =>
+              a.weight > b.weight ? -1 : 1
+            ),
+          }
+          setSettings(result)
+          updateStorage(result)
+        }
+
+        return {
+          barWeight: settings.barWeight,
+          availablePlates: settings.availablePlates,
+          weightSettings: settings,
+          updateBarWeight,
+          togglePlateAvailability,
+          updatePlateCount,
+          updated: settings.updated,
+          addPlate,
+        }
+      }
 
 export default useWeightSettings
